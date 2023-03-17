b9c32ce2fde2699a2ffe31836e2fdc86831bae68
x-dcx-x <petrisot.guillaume@gmail.com>
2023-03-17 15:11:51 +0100

    commit initial

--- lib/functions.php
+++ lib/functions.php
@@ -6,40 +6,40 @@ je vous ai créé la première qui est pour le moment incomplète et qui devra c
 la logique pour choisir la page à charger
 */
 
-function getContent() {
-	if(!isset($_GET['page'])){
-		include __DIR__.'/../pages/home.php';
-	}
-	elseif(isset($_GET['page']) && $_GET['page'] == "bio") {
+function getContent()
+{
+    if (!isset($_GET['page'])) {
+        include __DIR__ . '/../pages/home.php';
+    } elseif (isset($_GET['page']) && $_GET['page'] == "bio") {
 
-        include __DIR__.'/../pages/bio.php';
-    }
-	elseif(isset($_GET['page']) && $_GET['page'] == "contact") {
+        include __DIR__ . '/../pages/bio.php';
+    } elseif (isset($_GET['page']) && $_GET['page'] == "contact") {
 
-        include __DIR__.'/../pages/contact.php';
+        include __DIR__ . '/../pages/contact.php';
     }
 
 }
 
-function getPart($name) {
-	include __DIR__ . '/../parts/'. $name . '.php';
+function getPart($name)
+{
+    include __DIR__ . '/../parts/' . $name . '.php';
 }
 
-function getUserData (){
-    $content =  file_get_contents("../data/user.json");
+function getUserData()
+{
+    $content = file_get_contents("../data/user.json");
     $contents = json_decode($content);
-    foreach ( $contents as $key => $value){
-//        if(is_array($value)){
-        echo $key."<br>";
-        echo $value."<br>";
-    }
-        foreach ($value as $array) {
-            foreach ($array as $items => $valueDeux) {
-                echo "<p class='user'><span class='bold'>$items :</span> $valueDeux </p>";
+
+    foreach ($contents as $key => $value) {
+        if (is_array($value)) {
+            echo "<p class='identity'> $key </p>";
+            foreach ($value as $array) {
+                foreach ($array as $items => $valueDeux) {
+                    echo "<p class='user'><span class='bold'>$items :</span> $valueDeux </p>";
+                }
             }
-
+        } else {
+            echo "<p class='user'> <span class='bold'>$key :</span> $value</p>";
         }
-//
-
-
+    }
 }
