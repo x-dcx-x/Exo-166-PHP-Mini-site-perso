52b66823b69143461cf6b7ff181d5d8dd3ed9c00
x-dcx-x <petrisot.guillaume@gmail.com>
2023-03-17 16:01:56 +0100

    commit initial

--- public/save.php
+++ public/save.php
@@ -1,4 +1,10 @@
 <?php
+echo "Username:".$_POST["username"]."<br>";
+echo "Age:".$_POST["age"]."<br>";
+echo "Password:".$_POST["password"]."<br>";
+echo "Password Repeat:".$_POST["password repeat"]."<br>";
+echo "Username:".$_POST["username"]."<br>";
+
 
 $jsonMessage = file_put_contents("../data/last_message.json", $_POST);
 json_encode($jsonMessage);
